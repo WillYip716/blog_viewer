5cc75a56867b2a516f99923efb0aa35e409b2742
WillYip716 <william.yip.716@gmail.com>
2020-07-18 21:36:41 -0700

    setup for axios and first test for get posts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route,Link } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import Post from './components/post';
 import Home from './components/home';
 import About from './components/about';
--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,36 @@
 import React from 'react'
+import axios from 'axios';
 
-function Home(){
-    return(
-        <div>
-            <h1>Hello from home</h1>
-        </div>
-    )
+class Home extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state ={
+            loading: true,
+            posts: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('http://10.0.2.15:4000/posts')
+          .then(res => {
+              console.log(res.data);
+            //const persons = res.data;
+            /*this.setState((state) => ({
+                loading: false,
+                posts: res.data
+            }));  */
+          })
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Hello from home</h1>
+            </div>
+        )
+    }
+    
 }
 
 export default Home;
